a230c2301ab01072e0bdc002de49ac91ade27226
root <youremail@example.com>
2024-05-11 08:12:13 +0000

    resource_tracker file for AWS

---
+++ aws_resource_treacker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This Script will report AWS resource usage
+#############################################
+
+set -x
+
+# AWS S3
+# AWS EC2
+# AWS Lambda
+# AWS IAM Users
+
+
+# List S3 Buckets
+echo "Print S3 buckets" >> resource_tracker
+aws s3 ls >> resource_tracker
+
+# List EC2 instances
+echo "Print EC2 Instances" >> resource_tracker
+# aws ec2 describe-instances
+aws ec2 describe-instances | jq ' .Reservations[].Instances[].InstanceId' >> resource_tracker
+
+
+# List lambda
+echo "Print Lamda functions" >> resource_tracker
+aws lambda list-functions >> resource_tracker
+
+# List IAM Users
+echo "Print List of IAM Users" >> resource_tracker
+aws iam list-users >> resource_tracker
+
